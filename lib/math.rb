893ba5b6bb9d35ea1df3eb093ac11982419dd08e
juliochavesnyc <juliochavesnyc@gmail.com>
2020-11-27 04:02:59 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -6,24 +6,28 @@ addition(5, 4)
 
 def subtraction(num1, num2)
   num2 - num1
+  subtraction
 end
 
 subtraction(1, 5)
 
 def division(num1, num2)
   num2 / num1
+  division
 end
 
 division(2, 8)
 
 def multiplication(num1, num2)
   num1 * num2
+  multiplication
 end
 
 multiplication(4, 2)
 
 def modulo(num1, num2)
   num2 % num1
+  modulo
 end
 
 modulo(10, 3)
