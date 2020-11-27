8579d0d8472099420d1f09c37dfca605cd7309af
juliochavesnyc <juliochavesnyc@gmail.com>
2020-11-27 03:59:37 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,20 +5,28 @@ end
 addition(5, 4)
 
 def subtraction(num1, num2)
-
+  num2 - num1
 end
+
+subtraction(1, 5)
 
 def division(num1, num2)
-
+  num2 / num1
 end
+
+division(2, 8)
 
 def multiplication(num1, num2)
-
+  num1 * num2
 end
+
+multiplication(4, 2)
 
 def modulo(num1, num2)
-
+  num2 % num1
 end
+
+modulo(10, 3)
 
 def square_root(num)
 
